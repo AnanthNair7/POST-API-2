7bbf4e2b70fb88b6ef04e149bf6f7b5eaaa9b9a5
Ananth Nair <ananthnair96@gmail.com>
2023-08-17 09:29:38 +0530

    Inital commit

---
+++ Task-Api-Intergration-Post-2/StructViewController.swift
@@ -0,0 +1,12 @@
+//
+//  StructViewController.swift
+//  Task-Api-Intergration-Post-2
+//
+//  Created by Ananth Nair on 17/08/23.
+//
+
+import UIKit
+
+struct Root : Codable {
+    var variantList : [String?]
+}
